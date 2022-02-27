711ec303522afcabee7b86c93c2b715b87773aa0
Lichen Yu <lichen@Lichens-MacBook-Pro.local>
2022-02-26 22:14:19 -0500

    Finish Main Activity Front + Backend with Google sign in + Firebase realtime database. More backend code will be pushed later.

--- app/src/main/java/uwaterloo/cs446group7/increpeable/databaseClasses/User.java
+++ app/src/main/java/uwaterloo/cs446group7/increpeable/databaseClasses/User.java
@@ -1,9 +1,8 @@
 package uwaterloo.cs446group7.increpeable.databaseClasses;
 
-
 public class User {
+    private String email;
     private String username;
-    private String password;
     private String description;
     private String profileImageURL;
     private Integer numLikes = 0;
@@ -15,20 +14,21 @@ public class User {
 
     public User() {}
 
-    public User(String username, String password, String description, String profileImageURL) {
+    public User(String email, String username, String description, String profileImageURL) {
+        this.email = email;
         this.username = username;
-        this.password = password;
         this.description = description;
         this.profileImageURL = profileImageURL;
     }
 
     // getters
     public String getUsername() { return username; }
-    public String getPassword() { return password; }
     public String getDescription() { return description; }
     public String getProfileImageURL() { return profileImageURL; }
     public Integer getNumLikes() { return numLikes; }
     public Integer getNumFollowers() { return numFollowers; }
+    public String getEmail() { return email; }
+
 //    public RealmList<ObjectId> getMyPostIDs() { return myPostIDs; }
 //    public RealmList<ObjectId> getCollectedPostIDs() { return collectedPostIDs; }
 //    public RealmList<ObjectId> getFollowerIDs() { return followerIDs; }
@@ -36,11 +36,12 @@ public class User {
 
     // setters
     public void setUsername(String username) { this.username = username; }
-    public void setPassword(String password) { this.password = password; }
     public void setDescription(String description) { this.description = description; }
     public void setProfileImageURL(String profileImageURL) { this.profileImageURL = profileImageURL; }
     public void setNumLikes(Integer numLikes) { this.numLikes = numLikes; }
     public void setNumFollowers(Integer numFollowers) { this.numFollowers = numFollowers; }
+    public void setEmail(String email) { this.email = email; }
+
 //    public void setMyPostIDs(RealmList<ObjectId> myPostIDs) { this.myPostIDs = myPostIDs; }
 //    public void setCollectedPostIDs(RealmList<ObjectId> collectedPostIDs) { this.collectedPostIDs = collectedPostIDs; }
 //    public void setFollowerIDs(RealmList<ObjectId> followerIDs) { this.followerIDs = followerIDs; }
