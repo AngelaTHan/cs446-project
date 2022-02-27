21f00f21a5a5f415db56242ea85e731602cdbad9
Guanlin  Cheng <guanlincheng@GuanlindeMacBook-Pro.local>
2022-02-26 23:06:08 -0500

    Home Page UI Done

---
+++ app/src/main/java/uwaterloo/cs446group7/increpeable/homeActivity.java
@@ -0,0 +1,14 @@
+package uwaterloo.cs446group7.increpeable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
