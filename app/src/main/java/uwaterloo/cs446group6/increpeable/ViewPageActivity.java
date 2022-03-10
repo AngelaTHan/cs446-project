8fc9a263040943965da0b3664c2224b8b93f5862
AngelaTHan <55663082+AngelaTHan@users.noreply.github.com>
2022-03-09 20:31:05 -0500

    allowed like and collect ui

--- app/src/main/java/uwaterloo/cs446group6/increpeable/ViewPageActivity.java
+++ app/src/main/java/uwaterloo/cs446group6/increpeable/ViewPageActivity.java
@@ -1,8 +1,12 @@
 package uwaterloo.cs446group6.increpeable;
 
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -14,6 +18,10 @@ public class ViewPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ColorStateList redStateList = ContextCompat.getColorStateList(getBaseContext(), R.color.red);
+        ColorStateList darkGrayStateList = ContextCompat.getColorStateList(getBaseContext(), R.color.dark_gray);
+        ColorStateList yellowStateList = ContextCompat.getColorStateList(getBaseContext(), R.color.yellow);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
 
@@ -21,19 +29,50 @@ public class ViewPageActivity extends AppCompatActivity {
         followButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View view) {
-                int red = ContextCompat.getColor(getBaseContext(), R.color.red);
-                int darkGray = ContextCompat.getColor(getBaseContext(), R.color.dark_gray);
                 if (hasFollowed) {
                     followButton.setText("Follow");
-                    followButton.setBackgroundColor(red);
+                    followButton.setBackgroundTintList(redStateList);
                     hasFollowed = false;
                 } else {
                     followButton.setText("Following");
-                    followButton.setBackgroundColor(darkGray);
+                    followButton.setBackgroundTintList(darkGrayStateList);
                     hasFollowed = true;
                 }
             }
         });
-    }
 
+        LinearLayout likeButton = findViewById(R.id.like);
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                ImageView likeIcon = findViewById(R.id.likeIcon);
+                if (hasLiked) {
+                    likeIcon.setBackgroundResource(R.drawable.like);
+                    likeIcon.setBackgroundTintList(darkGrayStateList);
+                    hasLiked = false;
+                } else {
+                    likeIcon.setBackgroundResource(R.drawable.like_solid);
+                    likeIcon.setBackgroundTintList(redStateList);
+                    hasLiked = true;
+                }
+            }
+        });
+
+        LinearLayout collectButton = findViewById(R.id.collect);
+        collectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                ImageView collectIcon = findViewById(R.id.collectIcon);
+                if (hasCollected) {
+                    collectIcon.setBackgroundResource(R.drawable.collect);
+                    collectIcon.setBackgroundTintList(darkGrayStateList);
+                    hasCollected = false;
+                } else {
+                    collectIcon.setBackgroundResource(R.drawable.collect_solid);
+                    collectIcon.setBackgroundTintList(yellowStateList);
+                    hasCollected = true;
+                }
+            }
+        });
+    }
 }
